91b277e711e276acecbd5a44a72099839f0a4a11
Alex <162396898+CatBio908@users.noreply.github.com>
2026-01-12 12:59:44 -0600

    upload

---
+++ Assets/Glint/TestScene/DrawingTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DrawingTest : MonoBehaviour
+{
+
+	public List<Line> SceneData;
+
+    private void Start()
+    {
+        SceneData = new List<Line>();
+    }
+
+    void Update()
+    {
+		Vector3 mousePosition = Vector3.zero;
+
+		Mouse mouse = Mouse.current; 
+		if (mouse != null)
+        {
+			mousePosition = mouse.position.ReadValue(); 
+        }
+
+        SceneData.Add(new Line(new Vector3(0,			0,				0), mousePosition, Color.gray));
+        SceneData.Add(new Line(new Vector3(Screen.width, 0,				0), mousePosition, Color.red));
+        SceneData.Add(new Line(new Vector3(0,			Screen.height,	0), mousePosition, Color.green));
+        SceneData.Add(new Line(new Vector3(Screen.width, Screen.height,	0), mousePosition, Color.blue));
+
+
+		DrawScene();
+
+	}
+
+	public void DrawScene()
+	{
+		foreach (Line line in SceneData)
+		{
+			Glint.AddCommand(line);
+		}
+
+		SceneData.Clear();
+	}
+}
