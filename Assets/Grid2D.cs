a82bd0e9a12884f0436721dc9accab9addeb5f1f
Alex <162396898+CatBio908@users.noreply.github.com>
2026-02-04 14:21:52 -0600

    grid

--- Assets/Grid2D.cs
+++ Assets/Grid2D.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Grid2D : MonoBehaviour
 {
- 
+
     public Vector3 ScreenSize;
     public Vector3 origin;
 
-    public float OriginY;
-    public float OriginX;
+
+
+
 
     public float GridSize = 10f;
     public float MinGridSize = 2f;
@@ -19,10 +20,6 @@ public class Grid2D : MonoBehaviour
 
     public float OriginSize = .6f;
 
-    public float TopPoint;
-    public float RightPoint;
-    public float BottomPoint;
-    public float LeftPoint;
 
     public Color AxisColor = Color.white;
     public Color LineColor = Color.grey;
@@ -32,7 +29,7 @@ public class Grid2D : MonoBehaviour
     public bool isDrawingAxis = true;
     public bool isDrawingDivisions = true;
 
-    public bool isDrawing = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -40,31 +37,137 @@ public class Grid2D : MonoBehaviour
         ScreenSize = new Vector3(Screen.width, Screen.height);
         origin = new Vector3(Screen.width / 2, Screen.height / 2);
 
-        OriginY = Screen.height / 2;
-        OriginX = Screen.width / 2;
+
     }
 
     void Update()
     {
-        DrawAxis();
-        DrawOrigin();
-        DrawDivision();
+        GetInput();
+
         DrawGrid();
-        TopPoint = GridSize * OriginSize;
-        RightPoint = GridSize * OriginSize;
-        BottomPoint = GridSize * OriginSize;
-        LeftPoint = GridSize * OriginSize;
+
     }
 
     public Vector3 GridtoScene(Vector3 gridSpace)
     {
         return Vector3.zero;
-    } 
-    
+    }
+
     public Vector3 ScenetoGrid(Vector3 screenSpace)
     {
         return Vector3.zero;
     }
+
+    void GetInput()
+    {
+
+    }
+
+    public void DrawOrigin()
+    {
+        if (isDrawingOrigin)
+        {
+
+            Vector3 TopPoint = origin;
+            TopPoint.y += GridSize * OriginSize;
+            Vector3 RightPoint = origin;
+            RightPoint.x += GridSize * OriginSize;
+            Vector3 BottomPoint = origin;
+            BottomPoint.y -= GridSize * OriginSize;
+            Vector3 LeftPoint = origin;
+            LeftPoint.x -= GridSize * OriginSize;
+
+
+
+
+            DrawLine(TopPoint, RightPoint, AxisColor);
+            DrawLine(RightPoint, BottomPoint, AxisColor);
+            DrawLine(BottomPoint, LeftPoint, AxisColor);
+            DrawLine(LeftPoint, TopPoint, AxisColor);
+
+        }
+    }
+
+
+
+    public void DrawLineAtPoint(Vector3 point, Color drawingColor)
+    {
+
+        DrawLine(new Vector3(0, point.y, 0), new Vector3(Screen.width, point.y, 0), drawingColor);
+        DrawLine(new Vector3(point.x, 0, 0), new Vector3(point.x, Screen.height, 0), drawingColor);
+    }
+
+
+    public void DrawGrid()
+    {
+        bool isDrawing = true;
+        Vector3 PosPoint = Vector3.zero;
+        Vector3 NegPoint = Vector3.zero;
+        Vector3 PointOffset = Vector3.zero;
+
+        int indexOffset = 0;
+        Color drawColor = LineColor;
+
+
+        while (isDrawing)
+        {
+            //1. figure out what line to draw. 
+            // Default color to standard line 
+            drawColor = LineColor;
+
+            // Check for divison line 
+            if (isDrawingDivisions && ((indexOffset % DivisionCount) == 0))
+            {
+                drawColor = DivisionColor;
+            }
+
+            // Check for Axis 
+            if (isDrawingAxis && (indexOffset == 0))
+            {
+                drawColor = AxisColor;
+                DrawOrigin();
+            }
+
+            // 2. Figure out Points to draw on this go. 
+
+            PointOffset = new Vector3(GridSize, GridSize, 0) * indexOffset;
+
+            PosPoint = origin + PointOffset;
+            NegPoint = origin - PointOffset;
+
+            //3. Draw the points 
+
+            DrawLineAtPoint(PosPoint, drawColor);
+            DrawLineAtPoint(NegPoint, drawColor);
+
+
+
+
+            // 4. figure out when to stop drawing. 
+            indexOffset++;
+            if (indexOffset >= 10)
+            {
+                isDrawing = false;
+            }
+
+            if (NegPoint.x <= 0 || PosPoint.x >= Screen.width)
+            {
+                    isDrawing = false;
+            }
+            if (NegPoint.y <= 0 || PosPoint.y >= Screen.height)
+            {
+                isDrawing = false;
+            }
+
+            //test for x being off the screen on both sides
+            //test for y being off the screen on both sides
+
+        }
+    }
+
+
+
+
 
     /// <summary>
     /// Draws the given line object. If you are creating new line object, use the overload that takes parameters instead.
@@ -84,51 +187,5 @@ public class Grid2D : MonoBehaviour
     {
         Glint.AddCommand(new Line(start, end, color));
     }
-
-    public void DrawOrigin()
-    {
-        if (isDrawingOrigin == true)
-        {
-            DrawLine(new Vector3(0, TopPoint, 0) + origin, new Vector3(RightPoint, 0, 0) + origin , LineColor);
-            DrawLine(new Vector3(RightPoint, 0, 0) + origin, new Vector3(0, -BottomPoint, 0) + origin, LineColor);
-            DrawLine(new Vector3(0, -BottomPoint, 0) + origin, new Vector3(-LeftPoint, 0, 0) + origin, LineColor);
-            DrawLine(new Vector3(-LeftPoint, 0, 0) + origin, new Vector3(0, TopPoint, 0) + origin, LineColor);
-        }
-    }
-
-    public void DrawAxis()
-    {
-        if (isDrawingAxis == true)
-        {
-            DrawLine(new Vector3(0, OriginY, 0), new Vector3(Screen.width, OriginY, 0), AxisColor);
-            DrawLine(new Vector3(OriginX, 0, 0), new Vector3(OriginX, Screen.height, 0), AxisColor);
-        }
-
-    }
-
-    public void DrawGrid()
-    {
-        Vector3 PosPoint;
-        Vector3 NegPoint;
-
-        //indexOffset = 0;
-
-        while (isDrawing)
-        {
-
-        }
-    }
-
-    public void DrawDivision()
-    {
-        if (isDrawingDivisions == true)
-        {
-          /*  DrawLine(new Vector3(0, OriginY + DivisionCount, 0), new Vector3(Screen.width, OriginY + DivisionCount, 0), DivisionColor);
-            DrawLine(new Vector3(0, OriginY - DivisionCount, 0), new Vector3(Screen.width, OriginY - DivisionCount, 0), DivisionColor);
-            DrawLine(new Vector3(OriginX + DivisionCount, 0, 0), new Vector3(OriginX + DivisionCount, Screen.height, 0), DivisionColor);
-            DrawLine(new Vector3(OriginX - DivisionCount, 0, 0), new Vector3(OriginX - DivisionCount, Screen.height, 0), DivisionColor); */
-        }
-    }
-
 
 }
